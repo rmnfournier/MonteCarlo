50237f0bd6b40f8ec3c5ef573e06011db166fdef
Romain Fournier <rmn.fournier@gmail.com>
2019-02-05 11:37:10 +0100

    Feature : Basic implementation of MonteCarlo Algorithm

--- MonteCarlo.cpp
+++ MonteCarlo.cpp
@@ -1,5 +1,82 @@
-//
-// Created by romain on 05/02/19.
-//
+/**
+ @file  MonteCarlo.cpp
+ @brief  Implementation of the methods common to all MonteCarlo simulations
+ @author  Romain Fournier
+ @date  05.02.2019
+**/
 
 #include "MonteCarlo.hh"
+#include <random>
+#include <ctime>
+
+void MonteCarlo::reset (){
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 generator(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::normal_distribution<double> distribution(0.0,initial_variance_);
+
+    for (auto& el : x_){
+        el = distribution(generator);
+    }
+}
+
+bool MonteCarlo::metropolis(){
+    bool accepted(false);
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    std::normal_distribution<double> distribution(0.0,step_variance_);
+    std::uniform_real_distribution<double> uniform(0,1);
+    std::uniform_int_distribution<int> uniint(0,N_-1);
+
+    vector<double>x_new(x_);
+    // Propose a new configuration x
+    for (unsigned int i(0);i<points_to_update_;i++){
+        //select slice to move
+        int slice = uniint(generator);
+        // generate a move
+        double delta_xi = distribution(generator);
+        x_new[slice]+=delta_xi;
+    }
+    // accepts it according to metropolis-hasting rule
+    if(p(x_new)/p(x_)>=uniform(generator)){
+        x_=std::move(x_new);
+        accepted=true;
+    }
+    return accepted;
+}
+
+unsigned int MonteCarlo::warmup(unsigned int nb_steps){
+    unsigned int accepted(0);
+    for (unsigned int i(0);i<nb_steps;i++){
+        if(metropolis()) accepted++; // Increment accepted if metropolis returns true, i.e. a change has been made
+    }
+    return accepted;
+}
+
+void MonteCarlo::sample(){
+    open_output_file();
+    // For each bloc
+    for(unsigned int sampling(0);sampling<nb_blocs_;sampling++){
+        // Initialize the mean and the variance for the current bloc
+        double bloc_mean(0);
+        reset();
+        warmup(warmup_steps_);
+
+        for(unsigned int bloc(0);bloc<bloc_size_;bloc++) {
+            // Perform some updates before sampling again
+            warmup(steps_between_samples_);
+            // Update the mean value of the term
+            bloc_mean+=sampling_term();
+        }
+        // Devide by the bloc_size to get the mean
+        bloc_mean/=(bloc_size_+0.0);
+        // Save the results for analysis
+        write_bloc_result(bloc_mean);
+    }
+    close_output_file();
+}
+
+void MonteCarlo::write_bloc_result(double bloc_mean){
+    outputfile_<<bloc_mean<<",";
+    outputfile_.flush();
+}
--- MonteCarlo.hh
+++ MonteCarlo.hh
@@ -1,13 +1,93 @@
-//
-// Created by romain on 05/02/19.
-//
+/**
+ @file  MonteCarlo.hh
+ @brief  Abstract class helping to implement Monte Carlo Methods
+ @author  Romain Fournier
+ @date  05.02.2019
+**/
 
 #ifndef MONTECARLO_MONTECARLO_HH
 #define MONTECARLO_MONTECARLO_HH
 
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 class MonteCarlo {
+public:
+    MonteCarlo(unsigned int N_, double initial_variance_, unsigned int bloc_size_, unsigned int nb_blocs_,
+               double step_variance_, unsigned int points_to_update_, unsigned int warmup_steps_,
+               unsigned int steps_between_samples_, const string &outputfile_name_, const ofstream &outputfile_) : N_(
+            N_), initial_variance_(initial_variance_), bloc_size_(bloc_size_), nb_blocs_(nb_blocs_), step_variance_(
+            step_variance_), points_to_update_(points_to_update_), warmup_steps_(warmup_steps_), steps_between_samples_(
+            steps_between_samples_), outputfile_name_(outputfile_name_) {}
 
+    /**
+     * @brief generate a random configuration x according to the initial_variance_ attribut
+     */
+    void reset();
+    /**
+     * @brief Compute the probability of a configuration
+     * @param x configuration to test
+     * @return double in (0,1) corresponding to the probability of the current configuration
+     */
+    virtual double p(const vector<double> & x) = 0;
+
+    /**
+     * @brief try to make a random modification to the attribut x, and accept it according to the Metropolis-Hasting algorithm
+     * @param -
+     * @return true if there was a modification, false otherwise
+     */
+    bool metropolis();
+    /**
+     * @brief Perform nb_steps updates according to the metropolis-hasting algorithm
+     * @param nb_steps number of changes that will be proposed
+     * @return Number of accepted changes
+     */
+    unsigned int warmup(unsigned int nb_steps);
+
+    /**
+     * @brief Define the term that has to be sampled
+     * @param -
+     * @return Value of the term in the current configuration
+     */
+    virtual double sampling_term()=0;
+
+    /**
+     * @brief Perform the MonteCarlo Simulation, save the results in outputfile_
+     */
+    void sample();
+
+    /**
+     * @brief Open the file that saves the results
+     */
+    void open_output_file() {outputfile_.open(outputfile_name_);}
+    /**
+    * @brief Open the file that saves the results
+    */
+    void close_output_file(){outputfile_.close();}
+
+    /**
+     * @brief Add a line to the output file
+     */
+    void write_bloc_result(double);
+
+protected:
+    vector<double> x_; /** actual configuration **/
+    unsigned int N_; /** Size of the vector x_ **/
+    double initial_variance_; /** Initial variance of the distribution x **/
+
+    unsigned int bloc_size_; /** size of a bloc Monte Carlo Bloc **/
+    unsigned int nb_blocs_; /** Number of blocs to sample **/
+
+    double step_variance_; /** Variance of the proposed steps **/
+    unsigned int points_to_update_ ; /** Number of points to update **/
+    unsigned int warmup_steps_; /** Number of updates in the warmup phase **/
+    unsigned int steps_between_samples_; /** Number of updates between two consecutive samplings **/
+
+    string outputfile_name_; /** Name of the outputfile */
+    ofstream outputfile_; /** Outputfile */
 };
 
 
