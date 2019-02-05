899c31438e9eb2c6b72bf7e62bdbb868c8f25f6c
Unknown <rmn.fournier@gmail.com>
2019-02-05 11:35:03 +0100

    Feature : Creation of the class

---
+++ MonteCarlo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by romain on 05/02/19.
+//
+
+#include "MonteCarlo.hh"
---
+++ MonteCarlo.hh
@@ -0,0 +1,14 @@
+//
+// Created by romain on 05/02/19.
+//
+
+#ifndef MONTECARLO_MONTECARLO_HH
+#define MONTECARLO_MONTECARLO_HH
+
+
+class MonteCarlo {
+
+};
+
+
+#endif //MONTECARLO_MONTECARLO_HH
